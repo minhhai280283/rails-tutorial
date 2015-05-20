7ce7f780d0c71356ed35c0564e7add00f0495577
minh hai <minhhai@gmail.com>
2015-05-20 15:39:44 +0700

    Make a basic User model(include secure password)

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'user/new'
 
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'sigup'   => 'user#new'
+  get 'sigup'   => 'users#new'
 end
---
+++ sample_app/db/migrate/20150520075945_add_password_disgest_to_users.rb
@@ -0,0 +1,5 @@
+class AddPasswordDisgestToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_disgest, :string
+  end
+end
